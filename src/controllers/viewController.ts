5a11cb684f5b382e7efa3ce854922598e446ab83
최인혁 <inhyuk04@gmail.com>
2021-12-06 16:16:57 +0900

    feat: show player avatar image from s3

--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -17,23 +17,39 @@ export const getSearch = async (req: Request, res: Response) => {
     playerID,
     player: undefined,
     chats: undefined,
+    playerErr: '',
+    chatErr: '',
   };
 
   if (playerID) {
     //find player in db
-    const user_url = new URL(
-      `http://DemoALB-1573559176.ap-northeast-2.elb.amazonaws.com:8000/user/info/${playerID}`
-      // `http://localhost:8000/user/info/${playerID}`
-    );
-    player = await (await fetch(user_url, { method: 'GET' })).json();
-    params.player = player;
+    try {
+      const user_url = new URL(
+        `http://DemoALB-1573559176.ap-northeast-2.elb.amazonaws.com:8000/user/info/${playerID}`
+        // `http://localhost:8000/user/info/${playerID}`
+      );
+      player = await (await fetch(user_url, { method: 'GET' })).json();
+      params.player = player;
+      player.avatarURL = `https://jbus-avatar-bucket.s3.ap-northeast-2.amazonaws.com/${player.playerModel}.png`;
+    } catch (err) {
+      params.playerErr =
+        '플레이어 정보 가져오기를 실패하였습니다. 잠시 후 다시 시도해주세요';
+    }
+
     //find player's chat in db
-    const chat_url = new URL(
-      `http://DemoALB-1573559176.ap-northeast-2.elb.amazonaws.com:8001/chat/search/${playerID}`
-      // `http://localhost:8001/chat/search/${playerID}`
-    );
-    chats = JSON.parse(await (await fetch(chat_url, { method: 'GET' })).json());
-    params.chats = chats;
+    try {
+      const chat_url = new URL(
+        `http://DemoALB-1573559176.ap-northeast-2.elb.amazonaws.com:8001/chat/search/${playerID}`
+        // `http://localhost:8001/chat/search/${playerID}`
+      );
+      chats = JSON.parse(
+        await (await fetch(chat_url, { method: 'GET' })).json()
+      );
+      params.chats = chats;
+    } catch (err) {
+      params.chatErr =
+        '채팅 정보 가져오기를 실패하였습니다. 잠시 후 다시 시도해주세요';
+    }
   }
 
   return res.render('search', params);
